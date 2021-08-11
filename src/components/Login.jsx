a9866906c2c6c35de9bd6188b1afbe01a93c495c
maruf-ahmed <ahmedmaruf474@gmail.com>
2021-08-11 16:26:23 +0600

    fixed the logout logic and the backend changed.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -30,7 +30,7 @@ export default function Login({login}) {
             setError('')
             setSuccess(false)
             try {
-                let res = await fetch('http://localhost:3131/users/login', {
+                let res = await fetch('https://programming-pair-back.herokuapp.com/users/login', {
                     method: 'POST',
                     headers: {
                         'Accept': 'application/json',
